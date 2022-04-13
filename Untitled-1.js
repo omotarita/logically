4c1e35c458269811812595bb48789699f30c50cf
omotarita <zcemoed@ucl.ac.uk>
2022-04-13 19:42:20 +0100

    Added character images + stories. Finishing touches

---
+++ Untitled-1.js
@@ -0,0 +1,28 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script>
+
+<script>
+
+    var typed4 = new Typed('.character-dialogue', {
+    strings: ['Hiya I\'m Cinderella!', 'Thanks for making the time', 'Let\'s do it again. Weee!'],
+    if typed5 = 
+    typeSpeed: 125,
+    backSpeed: 25,
+    bindInputFocusEvents: true,
+    loop: false,
+    cursorChar: '',
+    backDelay: 5000,
+    startDelay: 4000
+    });
+    
+    var typed5 = new Typed('.player-dialogue', {
+    strings: ['Hi I\'m HELPR', 'How are you feeling today?', 'Let\'s do it again. Weee!'],
+    typeSpeed: 125,
+    backSpeed: 25,
+    bindInputFocusEvents: true,
+    loop: false,
+    cursorChar: '',
+    backDelay: 5000,
+    startDelay: 1000
+    });
+    
+</script> 
